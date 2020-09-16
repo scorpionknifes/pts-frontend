24884b3f39cb34c6866f591a2e63b048b5481376
ZHENK <chengzhenyang@gmail.com>
2020-09-17 05:47:24 +1200

    Add turn into story query

--- src/GraphQL/Queries/StoryQuery.tsx
+++ src/GraphQL/Queries/StoryQuery.tsx
@@ -1,5 +1,6 @@
 import { gql } from '@apollo/client'
 import StoryFragment from '../Fragments/StoryFragment'
+import TurnFragment from '../Fragments/TurnFragment'
 
 const StoriesQuery = gql`
     query Stories {
@@ -14,9 +15,13 @@ const StoryQuery = gql`
     query Story($id: ID!) {
         story(id: $id) {
             ...StoryFragment
+            turns{
+                ...TurnFragment
+            }
         }
     }
     ${StoryFragment}
+    ${TurnFragment}
 `
 
 export { StoriesQuery, StoryQuery }
